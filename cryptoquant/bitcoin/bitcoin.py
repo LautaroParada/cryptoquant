ea50cd1ac8afe5b734bd7987494a3655ca9b6a3a
Lautaro Parada <lautaro.parada.opazo@gmail.com>
2025-10-09 17:40:32 -0300

    actualización y tnx endpoint
    Tuve que actualizar los nombres de todos los endpoints debido a que estos se repetiran despues para otros aspectos de btc (e.g. mineros). Esto permite legibilidad para el usuario. Esto se debe a la URL usada por la API para hacer la llamada de datos.

--- cryptoquant/bitcoin/bitcoin.py
+++ cryptoquant/bitcoin/bitcoin.py
@@ -9,14 +9,21 @@ from cryptoquant.request_handler_class import RequestHandler
 
 class Bitcoin(RequestHandler):
     def __init__(self, api_key: str):
-        self.ENTITY_URL = "btc/status/entity-list"
-        self.EXCHANGE_FLOWS = "btc/exchange-flows/reserve"
-        self.NETFLOW = "btc/exchange-flows/netflow"
-        self.INFLOW = "btc/exchange-flows/inflow"
-        self.OUTFLOW = "btc/exchange-flows/outflow"
-        super().__init__(api_key)
         
-    def get_bitcoin_entity(self, **query_params):
+        # URLS for exchanges
+        self.EXCH_ENTITY_URL = "btc/status/entity-list"
+        self.EXCH_RESERVES = "btc/exchange-flows/reserve"
+        self.EXCH_NETFLOW = "btc/exchange-flows/netflow"
+        self.EXCH_INFLOW = "btc/exchange-flows/inflow"
+        self.EXCH_OUTFLOW = "btc/exchange-flows/outflow"
+        self.EXCH_TNX_COUNT = "btc/exchange-flows/transactions-count"
+        super().__init__(api_key)
+    
+    #-------------------------------------
+    # Exchange endpoints
+    #-------------------------------------
+    
+    def get_btc_exch_entity(self, **query_params):
         """
         This method returns entity list to serve data. The meaning of the 
         market_type value of the exchange object is as follows:
@@ -44,9 +51,9 @@ class Bitcoin(RequestHandler):
             Entity list on a given type.
 
         """
-        return super().handle_request(self.ENTITY_URL, query_params)
+        return super().handle_request(self.EXCH_ENTITY_URL, query_params)
     
-    def get_bitcoin_reserve(self, **query_params):
+    def get_btc_exch_reserve(self, **query_params):
         """
         This endpoint returns the full historical on-chain balance 
         of Bitcoin exchanges.
@@ -84,9 +91,9 @@ class Bitcoin(RequestHandler):
             The amount of BTC on a given exchange on this window.
 
         """
-        return super().handle_request(self.EXCHANGE_FLOWS, query_params)
+        return super().handle_request(self.EXCH_RESERVES, query_params)
     
-    def get_bitcoin_netflow(self, **query_params):
+    def get_btc_exch_netflow(self, **query_params):
         """
         The difference between coins flowing into exchanges and flowing out of 
         exchanges. Netflow usually helps us to figure out an increase of idle 
@@ -125,9 +132,9 @@ class Bitcoin(RequestHandler):
             total netfow.
 
         """
-        return super().handle_request(self.NETFLOW, query_params)
+        return super().handle_request(self.EXCH_NETFLOW, query_params)
     
-    def get_bitcoin_inflow(self, **query_params):
+    def get_btc_exch_inflow(self, **query_params):
         """
         Inflow of BTC into exchange wallets for as far back as CQ track. 
         The average inflow is the average transaction value for transactions 
@@ -166,9 +173,9 @@ class Bitcoin(RequestHandler):
             inflow total, top 10 inflow, mean inflow, and 7 day sma inflow.
 
         """
-        return super().handle_request(self.INFLOW, query_params)
+        return super().handle_request(self.EXCH_INFLOW, query_params)
     
-    def get_bitcoin_outflow(self, **query_params):
+    def get_btc_exch_outflow(self, **query_params):
         """
         Outflow of BTC into exchange wallets for as far back as CQ track. 
         The average outflow is the average transaction value for transactions 
@@ -207,4 +214,45 @@ class Bitcoin(RequestHandler):
             outflow total, top 10 outflow, mean outflow, and 7 day sma outflow.
 
         """
-        return super().handle_request(self.OUTFLOW, query_params)
+        return super().handle_request(self.EXCH_OUTFLOW, query_params)
+    
+    def get_btc_exch_txn(self, **query_params):
+        """
+        This endpoint returns the number of transactions flowing in/out of 
+        Bitcoin exchanges.
+
+        Parameters
+        ----------
+        **query_params :
+            exchange (str, required): An exchange supported by CryptoQuant.
+            window (str, optional): day, hour, and block.
+            from_ (any, optional): This defines the starting time for which data
+                                will be gathered, formatted as YYYYMMDDTHHMMSS 
+                                (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                                If window=day is used, it can also be formatted 
+                                as YYYYMMDD (date). If window=block is used, you
+                                can also specify the exact block height (e.g. 510000). 
+                                If this field is not specified, response will 
+                                include data from the earliest time.
+           to_ (any, optinal): This defines the ending time for which data will
+                               be gathered, formatted as YYYYMMDDTHHMMSS 
+                               (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                               If window=day is used, it can also be formatted 
+                               as YYYYMMDD (date). If window=block is used, you
+                               can also specify the exact block height (e.g. 510000).
+                               If this field is not specified, response will 
+                               include data from the latest time
+           limit (int, optional): The maximum number of entries to return before
+                                  the latest data point (or before to if specified).
+                                  This field ranges from 1 to 100,000.
+           format (str, optional): A format type about return message type. 
+                                   Supported formats are json, csv
+
+        Returns
+        -------
+        dict
+            Transactions count inflows and transactions count outflow
+
+        """
+        
+        return super().handle_request(self.EXCH_TNX_COUNT, query_params)
