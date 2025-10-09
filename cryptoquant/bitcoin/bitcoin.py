afe7adb82d087d6249111b32e17bb41c7686b79d
Lautaro Parada <lautaro.parada.opazo@gmail.com>
2025-10-09 17:56:34 -0300

    In-House Flow endpoint

--- cryptoquant/bitcoin/bitcoin.py
+++ cryptoquant/bitcoin/bitcoin.py
@@ -18,6 +18,7 @@ class Bitcoin(RequestHandler):
         self.EXCH_OUTFLOW = "btc/exchange-flows/outflow"
         self.EXCH_TNX_COUNT = "btc/exchange-flows/transactions-count"
         self.EXCH_ADDRESSES_COUNT = "btc/exchange-flows/addresses-count"
+        self.EXCH_INHOUSE_FLOW = "btc/exchange-flows/in-house-flow"
         super().__init__(api_key)
     
     #-------------------------------------
@@ -297,3 +298,45 @@ class Bitcoin(RequestHandler):
 
         """
         return super().handle_request(self.EXCH_ADDRESSES_COUNT, query_params)
+    
+    def get_btc_exch_inhouseflow(self, **query_params):
+        """
+        This endpoint returns the in-house flow of BTC within wallets of the 
+        same exchange for as far back as CQ track. The average in-house flow is
+        the average transaction value for transactions flowing within wallets
+        on a given day.
+
+        Parameters
+        ----------
+        **query_params :
+            exchange (str, required): An exchange supported by CryptoQuant.
+            window (str, optional): day, hour, and block.
+            from_ (any, optional): This defines the starting time for which data
+                                will be gathered, formatted as YYYYMMDDTHHMMSS 
+                                (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                                If window=day is used, it can also be formatted 
+                                as YYYYMMDD (date). If window=block is used, you
+                                can also specify the exact block height (e.g. 510000). 
+                                If this field is not specified, response will 
+                                include data from the earliest time.
+           to_ (any, optinal): This defines the ending time for which data will
+                               be gathered, formatted as YYYYMMDDTHHMMSS 
+                               (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                               If window=day is used, it can also be formatted 
+                               as YYYYMMDD (date). If window=block is used, you
+                               can also specify the exact block height (e.g. 510000).
+                               If this field is not specified, response will 
+                               include data from the latest time
+           limit (int, optional): The maximum number of entries to return before
+                                  the latest data point (or before to if specified).
+                                  This field ranges from 1 to 100,000.
+           format (str, optional): A format type about return message type. 
+                                   Supported formats are json, csv
+
+        Returns
+        -------
+        dict
+            Total flow, mean flow, count transactions flow
+
+        """
+        return super().handle_request(self.EXCH_INHOUSE_FLOW, query_params)
