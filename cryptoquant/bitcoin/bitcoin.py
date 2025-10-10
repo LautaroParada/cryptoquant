0124933ab4bee5363c700035723f86e745abd57d
Lautaro Parada <lautaro.parada.opazo@gmail.com>
2025-10-10 10:52:39 -0300

    Mean coin age endpoint
    https://cryptoquant.com/docs#tag/BTC-Network-Indicator/operation/getMCA

--- cryptoquant/bitcoin/bitcoin.py
+++ cryptoquant/bitcoin/bitcoin.py
@@ -45,6 +45,7 @@ class Bitcoin(RequestHandler):
         self.NTW_NVM = "btc/network-indicator/nvm"
         self.NTW_PUELL_MULTIPLE = "btc/network-indicator/puell-multiple"
         self.NTW_COIN_DAYS_DESTROYED = "btc/network-indicator/cdd"
+        self.NTW_MEAN_COIN_AGE = "btc/network-indicator/mca"
         
         super().__init__(api_key)
     
@@ -1362,3 +1363,45 @@ class Bitcoin(RequestHandler):
 
         """
         return super().handle_request(self.NTW_COIN_DAYS_DESTROYED, query_params)
+    
+    def get_btc_ntw_mca(self, **query_params):
+        """
+        Mean Coin Age is the mean value of products of bitcoin unspent 
+        transaction output alive days and its value. It is basically similar to
+        cdd. But mca focuses on unspent transaction output. Mean Coin Dollar 
+        Age is the sum value of products of bitcoin unspent transaction output 
+        alive days, value, and price at the created time.
+
+        Parameters
+        ----------
+        **query_params : TYPE
+            window (str, optional): Currently, we only support day.
+            from_ (any, optional): This defines the starting time for which data
+                                will be gathered, formatted as YYYYMMDDTHHMMSS 
+                                (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                                If window=day is used, it can also be formatted 
+                                as YYYYMMDD (date). If window=block is used, you
+                                can also specify the exact block height (e.g. 510000). 
+                                If this field is not specified, response will 
+                                include data from the earliest time.
+           to_ (any, optinal): This defines the ending time for which data will
+                               be gathered, formatted as YYYYMMDDTHHMMSS 
+                               (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                               If window=day is used, it can also be formatted 
+                               as YYYYMMDD (date). If window=block is used, you
+                               can also specify the exact block height (e.g. 510000).
+                               If this field is not specified, response will 
+                               include data from the latest time
+           limit (int, optional): The maximum number of entries to return before
+                                  the latest data point (or before to if specified).
+                                  This field ranges from 1 to 100,000.
+           format (str, optional): A format type about return message type. 
+                                   Supported formats are json, csv
+
+        Returns
+        -------
+        dict
+            mca.
+
+        """
+        return super().handle_request(self.NTW_MEAN_COIN_AGE, query_params)
