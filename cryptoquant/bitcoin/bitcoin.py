612ada37f80181532868cc93cc4f1ec8eddb4bfc
Lautaro Parada <lautaro.parada.opazo@gmail.com>
2025-10-10 16:58:05 -0300

    miner 2 exchange flow endpoint

--- cryptoquant/bitcoin/bitcoin.py
+++ cryptoquant/bitcoin/bitcoin.py
@@ -71,6 +71,7 @@ class Bitcoin(RequestHandler):
         self.MINER_IN_HOUSE_FLOW = "btc/miner-flows/in-house-flow"
         # Bitcoin Inter Entity Flows
         self.INTER_EXCHANGE_TO_EXCHANGE = "btc/inter-entity-flows/exchange-to-exchange"
+        self.INTER_MINER_TO_EXCHANGE = "btc/inter-entity-flows/miner-to-exchange"
         
         super().__init__(api_key)
     
@@ -2397,3 +2398,48 @@ class Bitcoin(RequestHandler):
 
         """
         return super().handle_request(self.INTER_EXCHANGE_TO_EXCHANGE, query_params)
+    
+    def get_btc_inter_miner_2_exch(self, **query_params):
+        """
+        Metrics related to token flows from mining pools to exchanges. CQ 
+        provide several metrics, flow_total, the total number of tokens 
+        transferred from a mining pool to an exchange, flow_mean, the mean of 
+        tokens transferred, and transactions_count_flow, the number of 
+        transactions from a mining pool to an exchange.
+
+        Parameters
+        ----------
+        **query_params : TYPE
+            from_miner (str, required): A miner that CQ support.
+            to_exchange (str, required): An exchange that CQ support. This one
+                                        should not be the same as from_entity
+            window (str, optional): Currently, we only support day.
+            from_ (any, optional): This defines the starting time for which data
+                                will be gathered, formatted as YYYYMMDDTHHMMSS 
+                                (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                                If window=day is used, it can also be formatted 
+                                as YYYYMMDD (date). If window=block is used, you
+                                can also specify the exact block height (e.g. 510000). 
+                                If this field is not specified, response will 
+                                include data from the earliest time.
+           to_ (any, optinal): This defines the ending time for which data will
+                               be gathered, formatted as YYYYMMDDTHHMMSS 
+                               (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                               If window=day is used, it can also be formatted 
+                               as YYYYMMDD (date). If window=block is used, you
+                               can also specify the exact block height (e.g. 510000).
+                               If this field is not specified, response will 
+                               include data from the latest time
+           limit (int, optional): The maximum number of entries to return before
+                                  the latest data point (or before to if specified).
+                                  This field ranges from 1 to 100,000.
+           format (str, optional): A format type about return message type. 
+                                   Supported formats are json, csv.
+
+        Returns
+        -------
+        dict
+            from miner to exchange flow.
+
+        """
+        return super().handle_request(self.INTER_MINER_TO_EXCHANGE, query_params)
