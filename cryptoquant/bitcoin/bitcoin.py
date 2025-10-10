73934aafabaaff086eec1bf0fc3f5ac93de857a6
Lautaro Parada <lautaro.parada.opazo@gmail.com>
2025-10-10 13:51:20 -0300

    addresses count miner endpoint

--- cryptoquant/bitcoin/bitcoin.py
+++ cryptoquant/bitcoin/bitcoin.py
@@ -67,6 +67,7 @@ class Bitcoin(RequestHandler):
         self.MINER_INFLOW = "btc/miner-flows/inflow"
         self.MINER_OUTFLOW = "btc/miner-flows/outflow"
         self.MINER_TRANSACTIONS_COUNT = "btc/miner-flows/transactions-count"
+        self.MINER_ADDRESSES_COUNT = "btc/miner-flows/addresses-count"
         
         super().__init__(api_key)
     
@@ -2264,3 +2265,42 @@ class Bitcoin(RequestHandler):
 
         """
         return super().handle_request(self.MINER_TRANSACTIONS_COUNT, query_params)
+    
+    def get_btc_miner_addr_count(self, **query_params):
+        """
+        Number of addresses involved in inflow/outflow transactions.
+
+        Parameters
+        ----------
+        **query_params : TYPE
+            miner (str, required): A mining pool from the table that CQ support
+            window (str, optional): Currently, we only support day.
+            from_ (any, optional): This defines the starting time for which data
+                                will be gathered, formatted as YYYYMMDDTHHMMSS 
+                                (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                                If window=day is used, it can also be formatted 
+                                as YYYYMMDD (date). If window=block is used, you
+                                can also specify the exact block height (e.g. 510000). 
+                                If this field is not specified, response will 
+                                include data from the earliest time.
+           to_ (any, optinal): This defines the ending time for which data will
+                               be gathered, formatted as YYYYMMDDTHHMMSS 
+                               (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                               If window=day is used, it can also be formatted 
+                               as YYYYMMDD (date). If window=block is used, you
+                               can also specify the exact block height (e.g. 510000).
+                               If this field is not specified, response will 
+                               include data from the latest time
+           limit (int, optional): The maximum number of entries to return before
+                                  the latest data point (or before to if specified).
+                                  This field ranges from 1 to 100,000.
+           format (str, optional): A format type about return message type. 
+                                   Supported formats are json, csv.
+
+        Returns
+        -------
+        dict
+            Miner addresses count, in and out.
+
+        """
+        return super().handle_request(self.MINER_ADDRESSES_COUNT, query_params)
