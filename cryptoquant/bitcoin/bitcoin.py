1d7d0b9e268742b2e4c816d8fd8e923e3bdb0f8e
Lautaro Parada <lautaro.parada.opazo@gmail.com>
2025-10-10 12:39:37 -0300

    UTxO Realized Supply Distribution endpoint
    https://cryptoquant.com/docs#tag/BTC-Network-Indicator/operation/getUTxORealizedSupplyDistribution

--- cryptoquant/bitcoin/bitcoin.py
+++ cryptoquant/bitcoin/bitcoin.py
@@ -58,6 +58,7 @@ class Bitcoin(RequestHandler):
         self.NTW_UTXO_COUNT_AGE_DSTR = "btc/network-indicator/utxo-count-age-distribution"
         self.NTW_SPENT_OUTPUT_AGE_DSTR = "btc/network-indicator/spent-output-age-distribution"
         self.NTW_UTXO_SUPPLY_DSTR = "btc/network-indicator/utxo-supply-distribution"
+        self.NTW_UTXO_REALIZED_SUPPLY_DSTR = "btc/network-indicator/utxo-realized-supply-distribution"
         
         super().__init__(api_key)
     
@@ -1924,3 +1925,45 @@ class Bitcoin(RequestHandler):
 
         """
         return super().handle_request(self.NTW_UTXO_SUPPLY_DSTR, query_params)
+    
+    def get_btc_ntw_utxo_realized_supply_dstr(self, **query_params):
+        """
+        UTxO Realized Supply Distribution is a set of active supply with 
+        balance (supply) bands weighted by the price at UTxO created time. 
+        Similar to Realized Cap, this indicator summarizes the capitalization 
+        held by whales or retails (each band). We provide the distribution 
+        values in USD, and percent values.
+
+        Parameters
+        ----------
+        **query_params : TYPE
+            window (str, optional): Currently, we only support day.
+            from_ (any, optional): This defines the starting time for which data
+                                will be gathered, formatted as YYYYMMDDTHHMMSS 
+                                (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                                If window=day is used, it can also be formatted 
+                                as YYYYMMDD (date). If window=block is used, you
+                                can also specify the exact block height (e.g. 510000). 
+                                If this field is not specified, response will 
+                                include data from the earliest time.
+           to_ (any, optinal): This defines the ending time for which data will
+                               be gathered, formatted as YYYYMMDDTHHMMSS 
+                               (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                               If window=day is used, it can also be formatted 
+                               as YYYYMMDD (date). If window=block is used, you
+                               can also specify the exact block height (e.g. 510000).
+                               If this field is not specified, response will 
+                               include data from the latest time
+           limit (int, optional): The maximum number of entries to return before
+                                  the latest data point (or before to if specified).
+                                  This field ranges from 1 to 100,000.
+           format (str, optional): A format type about return message type. 
+                                   Supported formats are json, csv
+
+        Returns
+        -------
+        dict
+            UTxO Realized Supply Distribution.
+
+        """
+        return super().handle_request(self.NTW_UTXO_REALIZED_SUPPLY_DSTR, query_params)
