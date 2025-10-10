574b24974443eb96d6c1165d666444dfb6414625
Lautaro Parada <lautaro.parada.opazo@gmail.com>
2025-10-10 19:32:26 -0300

    addresses count endpoint

--- cryptoquant/bitcoin/bitcoin.py
+++ cryptoquant/bitcoin/bitcoin.py
@@ -93,6 +93,7 @@ class Bitcoin(RequestHandler):
         self.NETWORK_SUPPLY = "btc/network-data/supply"
         self.NETWORK_VELOCITY = "btc/network-data/velocity"
         self.NETWORK_TRANSACTIONS_COUNT = "btc/network-data/transactions-count"
+        self.NETWORK_ADDRESSES_COUNT = "btc/network-data/addresses-count"
         
         super().__init__(api_key)
     
@@ -3289,3 +3290,46 @@ class Bitcoin(RequestHandler):
 
         """
         return super().handle_request(self.NETWORK_TRANSACTIONS_COUNT, query_params)
+    
+    def get_btc_net_addr_count(self, **query_params):
+        """
+        This endpoint returns metrics relating to the number of used bitcoin 
+        addresses. We provide several metrics, addresses_count_active, the 
+        total number of unique addresses that were active (either sender or 
+        receiver) on the blockchain, addresses_count_sender, the number of 
+        addresses that were active as a sender, and addresses_count_receiver, 
+        the number of addresses that were active as a receiver.
+
+        Parameters
+        ----------
+        **query_params : TYPE
+            window (str, optional): Currently CQ support day, hour, block.
+            from_ (any, optional): This defines the starting time for which data
+                                will be gathered, formatted as YYYYMMDDTHHMMSS 
+                                (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                                If window=day is used, it can also be formatted 
+                                as YYYYMMDD (date). If window=block is used, you
+                                can also specify the exact block height (e.g. 510000). 
+                                If this field is not specified, response will 
+                                include data from the earliest time.
+           to_ (any, optinal): This defines the ending time for which data will
+                               be gathered, formatted as YYYYMMDDTHHMMSS 
+                               (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                               If window=day is used, it can also be formatted 
+                               as YYYYMMDD (date). If window=block is used, you
+                               can also specify the exact block height (e.g. 510000).
+                               If this field is not specified, response will 
+                               include data from the latest time
+           limit (int, optional): The maximum number of entries to return before
+                                  the latest data point (or before to if specified).
+                                  This field ranges from 1 to 100,000.
+           format (str, optional): A format type about return message type. 
+                                   Supported formats are json, csv.
+
+        Returns
+        -------
+        dict
+            Count of active, sender and receiver.
+
+        """
+        return super().handle_request(self.NETWORK_ADDRESSES_COUNT, query_params)
