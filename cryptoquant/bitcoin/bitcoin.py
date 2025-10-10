06c1f138b8eae5dfab14658949552028a4aece44
Lautaro Parada <lautaro.parada.opazo@gmail.com>
2025-10-10 19:39:59 -0300

    block bytes endpoint

--- cryptoquant/bitcoin/bitcoin.py
+++ cryptoquant/bitcoin/bitcoin.py
@@ -95,6 +95,7 @@ class Bitcoin(RequestHandler):
         self.NETWORK_TRANSACTIONS_COUNT = "btc/network-data/transactions-count"
         self.NETWORK_ADDRESSES_COUNT = "btc/network-data/addresses-count"
         self.NETWORK_TOKENS_TRANSFERRED = "btc/network-data/tokens-transferred"
+        self.NETWORK_BLOCK_BYTES = "btc/network-data/block-bytes"
         
         super().__init__(api_key)
     
@@ -3377,3 +3378,41 @@ class Bitcoin(RequestHandler):
 
         """
         return super().handle_request(self.NETWORK_TOKENS_TRANSFERRED, query_params)
+    
+    def get_btc_net_block_bytes(self, **query_params):
+        """
+        The mean size(in bytes) of all blocks generated.
+
+        Parameters
+        ----------
+        **query_params : TYPE
+            window (str, optional): Currently CQ support day, hour, block.
+            from_ (any, optional): This defines the starting time for which data
+                                will be gathered, formatted as YYYYMMDDTHHMMSS 
+                                (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                                If window=day is used, it can also be formatted 
+                                as YYYYMMDD (date). If window=block is used, you
+                                can also specify the exact block height (e.g. 510000). 
+                                If this field is not specified, response will 
+                                include data from the earliest time.
+           to_ (any, optinal): This defines the ending time for which data will
+                               be gathered, formatted as YYYYMMDDTHHMMSS 
+                               (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                               If window=day is used, it can also be formatted 
+                               as YYYYMMDD (date). If window=block is used, you
+                               can also specify the exact block height (e.g. 510000).
+                               If this field is not specified, response will 
+                               include data from the latest time
+           limit (int, optional): The maximum number of entries to return before
+                                  the latest data point (or before to if specified).
+                                  This field ranges from 1 to 100,000.
+           format (str, optional): A format type about return message type. 
+                                   Supported formats are json, csv.
+
+        Returns
+        -------
+        dict
+            The mean size (in bytes) of all blocks generated.
+
+        """
+        return super().handle_request(self.NETWORK_BLOCK_BYTES, query_params)
