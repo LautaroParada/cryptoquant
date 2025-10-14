201a071f45eeadab10b9a24d593805938fea5c9a
Lautaro Parada <lautaro.parada.opazo@gmail.com>
2025-10-14 10:54:15 -0300

    Entity list endpoint
    Se arreglo el problema de herencia en la clase madre crypto quant y ademas se implemento el endpoint entity list.

--- cryptoquant/ethereum/ethereum.py
+++ cryptoquant/ethereum/ethereum.py
@@ -9,4 +9,39 @@ from cryptoquant.request_handler_class import RequestHandler
 
 class Ethereum(RequestHandler):
     def __init__(self, api_key: str):
+        
+        #Entity List
+        self.ENTITY_STATUS = "eth/status/entity-list"
         super().__init__(api_key)
+        
+    # -------------------------------
+    # Entity list
+    # -------------------------------
+    
+    def get_eth_entity_list(self, **query_params):
+        """
+        This endpoint returns entity list to serve data. The meaning of the 
+        market_type value of the exchange object is as follows. For exchange 
+        objects, the market_type field tells whether the exchange is a spot 
+        exchange or a derivative exchange. Entities without a market type, 
+        such as miners or banks, will return 0 for market_type.
+
+        Exchange Market Type	Description
+                            0	Undefined
+                            1	Spot Exchange
+                            2	Derivative Exchange
+
+        Parameters
+        ----------
+        **query_params :
+            type_ (str, required): A type from the entity in exchange.
+            format_ (str:optional): A format type about return message type. 
+                            Supported formats are json, csv. Default is json
+
+        Returns
+        -------
+        dict
+            Entity list on a given type.
+
+        """
+        return super().handle_request(self.ENTITY_STATUS, query_params)
