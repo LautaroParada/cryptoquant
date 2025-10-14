7e4b51d8c9aba43cac543d79fe784f8c13fb63f5
Lautaro Parada <lautaro.parada.opazo@gmail.com>
2025-10-14 12:17:45 -0300

    staking validator total

--- cryptoquant/ethereum/ethereum.py
+++ cryptoquant/ethereum/ethereum.py
@@ -27,6 +27,7 @@ class Ethereum(RequestHandler):
         self.ETH_2_TOTAL_VALUE_STAKED = "eth/eth2/total-value-staked"
         self.ETH_2_STAKING_INFLOW_TOTAL = "eth/eth2/staking-inflow-total"
         self.ETH_2_STAKING_TRX_COUNT = "eth/eth2/staking-transaction-count"
+        self.ETH_2_STAKING_VALIDATOR_TOTAL = "eth/eth2/staking-validator-total"
         
         super().__init__(api_key)
         
@@ -524,3 +525,41 @@ class Ethereum(RequestHandler):
 
         """
         return super().handle_request(self.ETH_2_STAKING_TRX_COUNT, query_params)
+    
+    def get_eth_20_staking_validator_total(self, **query_params):
+        """
+        This endpoint returns the number of total validators.
+
+        Parameters
+        ----------
+        **query_params : TYPE
+            window (str, optional): day, hour, and block.
+            from_ (any, optional): This defines the starting time for which data
+                                will be gathered, formatted as YYYYMMDDTHHMMSS 
+                                (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                                If window=day is used, it can also be formatted 
+                                as YYYYMMDD (date). If window=block is used, you
+                                can also specify the exact block height (e.g. 510000). 
+                                If this field is not specified, response will 
+                                include data from the earliest time.
+           to_ (any, optinal): This defines the ending time for which data will
+                               be gathered, formatted as YYYYMMDDTHHMMSS 
+                               (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                               If window=day is used, it can also be formatted 
+                               as YYYYMMDD (date). If window=block is used, you
+                               can also specify the exact block height (e.g. 510000).
+                               If this field is not specified, response will 
+                               include data from the latest time
+           limit (int, optional): The maximum number of entries to return before
+                                  the latest data point (or before to if specified).
+                                  This field ranges from 1 to 100,000.
+           format (str, optional): A format type about return message type. 
+                                   Supported formats are json, csv
+
+        Returns
+        -------
+        dict
+            The number of the number of total validators on this window.
+
+        """
+        return super().handle_request(self.ETH_2_STAKING_VALIDATOR_TOTAL, query_params)
