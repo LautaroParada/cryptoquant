f2e1594e25e236eafaec32d32f011ff1a819b649
Lautaro Parada <lautaro.parada.opazo@gmail.com>
2025-10-14 11:28:23 -0300

    inflow

--- cryptoquant/ethereum/ethereum.py
+++ cryptoquant/ethereum/ethereum.py
@@ -15,6 +15,7 @@ class Ethereum(RequestHandler):
         # ETH Exchange Flows
         self.EXCH_FLOWS_RESERVE = "eth/exchange-flows/reserve"
         self.EXCH_FLOWS_NETFLOW = "eth/exchange-flows/netflow"
+        self.EXCH_FLOWS_INFLOW = "eth/exchange-flows/inflow"
         
         super().__init__(api_key)
         
@@ -133,3 +134,44 @@ class Ethereum(RequestHandler):
 
         """
         return super().handle_request(self.EXCH_FLOWS_NETFLOW, query_params)
+    
+    def get_eth_exch_inflow(self, **query_params):
+        """
+        This endpoint returns the inflow of ETH into exchange wallets for as 
+        far back as CQ track. The average inflow is the average transaction 
+        value for transactions flowing into exchange wallets on a given day.
+
+        Parameters
+        ----------
+        **query_params : TYPE
+            exchange (str, required): An exchange supported by CryptoQuant.
+            window (str, optional): day, hour, and block.
+            from_ (any, optional): This defines the starting time for which data
+                                will be gathered, formatted as YYYYMMDDTHHMMSS 
+                                (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                                If window=day is used, it can also be formatted 
+                                as YYYYMMDD (date). If window=block is used, you
+                                can also specify the exact block height (e.g. 510000). 
+                                If this field is not specified, response will 
+                                include data from the earliest time.
+           to_ (any, optinal): This defines the ending time for which data will
+                               be gathered, formatted as YYYYMMDDTHHMMSS 
+                               (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                               If window=day is used, it can also be formatted 
+                               as YYYYMMDD (date). If window=block is used, you
+                               can also specify the exact block height (e.g. 510000).
+                               If this field is not specified, response will 
+                               include data from the latest time
+           limit (int, optional): The maximum number of entries to return before
+                                  the latest data point (or before to if specified).
+                                  This field ranges from 1 to 100,000.
+           format (str, optional): A format type about return message type. 
+                                   Supported formats are json, csv
+
+        Returns
+        -------
+        dict
+            inflow statistics.
+
+        """
+        return super().handle_request(self.EXCH_FLOWS_INFLOW, query_params)
