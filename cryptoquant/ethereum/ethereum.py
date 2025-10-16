dad46dc4b7d22880c31fb019595edfa7d9996349
Lautaro Parada <lautaro.parada.opazo@gmail.com>
2025-10-16 12:34:49 -0300

    trx count all

--- cryptoquant/ethereum/ethereum.py
+++ cryptoquant/ethereum/ethereum.py
@@ -54,6 +54,7 @@ class Ethereum(RequestHandler):
         self.NETWORK_CONTRACT_CALLS_EXTERNAL = "eth/network-data/contract-calls-count-external"
         self.NETWORK_CONTRACT_CALLS_INTERNAL = "eth/network-data/contract-calls-count-internal"
         self.NETWORK_CONTRACT_CALLS_COUNT = "eth/network-data/contract-calls-count"
+        self.NETWORK_TRANSACTIONS_COUNT_ALL = "eth/network-data/transactions-count-all"
         
         super().__init__(api_key)
         
@@ -1604,7 +1605,46 @@ class Ethereum(RequestHandler):
         Returns
         -------
         dict
-            Contract call count total and mean..
+            Contract call count total and mean.
 
         """
         return super().handle_request(self.NETWORK_CONTRACT_CALLS_COUNT, query_params)
+    
+    def get_eth_ntx_trx_count_all(self, **query_params):
+        """
+        This endpoint returns metrics related to the number of transactions 
+        including internal contract calls.
+
+        Parameters
+        ----------
+        **query_params : TYPE
+            window (str, optional): day, hour, 10minute, and block.
+            from_ (any, optional): This defines the starting time for which data
+                                will be gathered, formatted as YYYYMMDDTHHMMSS 
+                                (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                                If window=day is used, it can also be formatted 
+                                as YYYYMMDD (date). If window=block is used, you
+                                can also specify the exact block height (e.g. 510000). 
+                                If this field is not specified, response will 
+                                include data from the earliest time.
+           to_ (any, optinal): This defines the ending time for which data will
+                               be gathered, formatted as YYYYMMDDTHHMMSS 
+                               (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                               If window=day is used, it can also be formatted 
+                               as YYYYMMDD (date). If window=block is used, you
+                               can also specify the exact block height (e.g. 510000).
+                               If this field is not specified, response will 
+                               include data from the latest time
+           limit (int, optional): The maximum number of entries to return before
+                                  the latest data point (or before to if specified).
+                                  This field ranges from 1 to 100,000.
+           format (str, optional): A format type about return message type. 
+                                   Supported formats are json, csv.
+
+        Returns
+        -------
+        dict
+            Trasactions count total and mean.
+
+        """
+        return super().handle_request(self.NETWORK_TRANSACTIONS_COUNT_ALL, query_params)
