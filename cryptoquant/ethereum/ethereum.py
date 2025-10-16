d048f815dbc80543f4744ac9af8e388e273cff47
Lautaro Parada <lautaro.parada.opazo@gmail.com>
2025-10-16 12:04:10 -0300

    velocity

--- cryptoquant/ethereum/ethereum.py
+++ cryptoquant/ethereum/ethereum.py
@@ -47,6 +47,7 @@ class Ethereum(RequestHandler):
         self.MARKET_CAPITALIZATION = "eth/market-data/capitalization"
         # ETH Network Data
         self.NETWORK_SUPPLY = "eth/network-data/supply"
+        self.NETWORK_VELOCITY = "eth/network-data/velocity"
         
         super().__init__(api_key)
         
@@ -1320,3 +1321,46 @@ class Ethereum(RequestHandler):
 
         """
         return super().handle_request(self.NETWORK_SUPPLY, query_params)
+    
+    def get_eth_ntx_velocity(self, **query_params):
+        """
+        This endpoint returns metrics related to the velocity of Ethereum. 
+        Ethereum's velocity is calculated by dividing the trailing 1 year 
+        estimated transaction volume(the cumulated sum of transferred tokens) 
+        by current supply. Velocity is a metric that explains how actively is 
+        money circulating in the market.
+
+        Parameters
+        ----------
+        **query_params : TYPE
+            window (str, optional): day, hour, 10minute, and block.
+            from_ (any, optional): This defines the starting time for which data
+                                will be gathered, formatted as YYYYMMDDTHHMMSS 
+                                (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                                If window=day is used, it can also be formatted 
+                                as YYYYMMDD (date). If window=block is used, you
+                                can also specify the exact block height (e.g. 510000). 
+                                If this field is not specified, response will 
+                                include data from the earliest time.
+           to_ (any, optinal): This defines the ending time for which data will
+                               be gathered, formatted as YYYYMMDDTHHMMSS 
+                               (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                               If window=day is used, it can also be formatted 
+                               as YYYYMMDD (date). If window=block is used, you
+                               can also specify the exact block height (e.g. 510000).
+                               If this field is not specified, response will 
+                               include data from the latest time
+           limit (int, optional): The maximum number of entries to return before
+                                  the latest data point (or before to if specified).
+                                  This field ranges from 1 to 100,000.
+           format (str, optional): A format type about return message type. 
+                                   Supported formats are json, csv.
+
+        Returns
+        -------
+        dict
+            Ethereum transaction volume in the trailing 1 year divided by
+            current total supply.
+
+        """
+        return super().handle_request(self.NETWORK_VELOCITY, query_params)
