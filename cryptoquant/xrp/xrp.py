aa61c2b88f17768fb4a6894321238a04f645f50b
Lautaro Parada <lautaro.parada.opazo@gmail.com>
2025-10-17 19:05:39 -0300

    Update xrp.py

--- cryptoquant/xrp/xrp.py
+++ cryptoquant/xrp/xrp.py
@@ -21,6 +21,8 @@ class XRP(RequestHandler):
         self.ENTITY_OUTFLOW = "xrp/entity-flows/outflow"
         self.ENTITY_ADDRESSES_COUNT = "xrp/entity-flows/addresses-count"
         self.ENTITY_WHALE_MOVEMENTS = "xrp/entity-flows/whale-movements"
+        # Flow Indicators
+        self.FLOW_EXCHANGE_INFLOW_VALUE_DISTRIBUITION = "xrp/flow-indicator/exchange-inflow-value-distribution"
         
     # -----------------------------------
     # Entity list
@@ -332,3 +334,49 @@ class XRP(RequestHandler):
 
         """
         return super().handle_request(self.ENTITY_WHALE_MOVEMENTS, query_params)
+    
+    # -----------------------------------
+    # XRP Flow Indicator
+    # -----------------------------------
+    
+    def get_xrp_flow_exch_inflow_value_dstr(self, **query_params):
+        """
+        Exchange Inflow Value Distribution is a metric that shows the amount 
+        distribution of xrp tokens flowed into exchange wallets according to 
+        its value.
+
+        Parameters
+        ----------
+        **query_params : TYPE
+            exchange (str, required): An exchange supported by CryptoQuant.
+            window (str, optional): day and hour.
+            from_ (any, optional): This defines the starting time for which data
+                                will be gathered, formatted as YYYYMMDDTHHMMSS 
+                                (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                                If window=day is used, it can also be formatted 
+                                as YYYYMMDD (date). If window=block is used, you
+                                can also specify the exact block height (e.g. 510000). 
+                                If this field is not specified, response will 
+                                include data from the earliest time.
+           to_ (any, optinal): This defines the ending time for which data will
+                               be gathered, formatted as YYYYMMDDTHHMMSS 
+                               (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                               If window=day is used, it can also be formatted 
+                               as YYYYMMDD (date). If window=block is used, you
+                               can also specify the exact block height (e.g. 510000).
+                               If this field is not specified, response will 
+                               include data from the latest time
+           limit (int, optional): The maximum number of entries to return before
+                                  the latest data point (or before to if specified).
+                                  This field ranges from 1 to 100,000.
+           format (str, optional): A format type about return message type. 
+                                   Supported formats are json, csv
+
+        Returns
+        -------
+        dict
+            Exchange inflows distribution band by the amount of the transferred 
+            tokens (xrp) at once.
+
+        """
+        return super().handle_request(self.FLOW_EXCHANGE_INFLOW_VALUE_DISTRIBUITION, query_params)
