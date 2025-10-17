269a28b2b38e26c28add7fe10178094b42dcfd01
Lautaro Parada <lautaro.parada.opazo@gmail.com>
2025-10-17 13:38:00 -0300

    failed tokens transferred count

--- cryptoquant/ethereum/ethereum.py
+++ cryptoquant/ethereum/ethereum.py
@@ -69,6 +69,7 @@ class Ethereum(RequestHandler):
         self.NETWORK_TOKENS_TRANSFERRED_BY_CONTRACT_CALLS = "eth/network-data/tokens-transferred-by-contract-calls"
         self.NETWORK_TOKENS_TRANSFERRED_ALL = "eth/network-data/tokens-transferred-all"
         self.NETWORK_FAILED_TRANSACTIONS_COUNT = "eth/network-data/failed-transactions-count"      
+        self.NETWORK_FAILED_TOKENS_TRANSFERRED_COUNT = "eth/network-data/failed-tokens-transferred-count"
         
         super().__init__(api_key)
         
@@ -2228,3 +2229,44 @@ class Ethereum(RequestHandler):
 
         """
         return super().handle_request(self.NETWORK_FAILED_TRANSACTIONS_COUNT, query_params)
+    
+    def get_eth_ntx_failed_tokens_transferred_count(self, **query_params):
+        """
+        This endpoint returns metrics related to the number of failed 
+        transactions with tokens transferred. We provide 
+        failed_tokens_transferred_count_total metric, the total number of 
+        failed transactions with tokens transferred.
+
+        Parameters
+        ----------
+        **query_params : TYPE
+            window (str, optional): day, hour, 10minute, and block.
+            from_ (any, optional): This defines the starting time for which data
+                                will be gathered, formatted as YYYYMMDDTHHMMSS 
+                                (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                                If window=day is used, it can also be formatted 
+                                as YYYYMMDD (date). If window=block is used, you
+                                can also specify the exact block height (e.g. 510000). 
+                                If this field is not specified, response will 
+                                include data from the earliest time.
+           to_ (any, optinal): This defines the ending time for which data will
+                               be gathered, formatted as YYYYMMDDTHHMMSS 
+                               (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                               If window=day is used, it can also be formatted 
+                               as YYYYMMDD (date). If window=block is used, you
+                               can also specify the exact block height (e.g. 510000).
+                               If this field is not specified, response will 
+                               include data from the latest time
+           limit (int, optional): The maximum number of entries to return before
+                                  the latest data point (or before to if specified).
+                                  This field ranges from 1 to 100,000.
+           format (str, optional): A format type about return message type. 
+                                   Supported formats are json, csv.
+
+        Returns
+        -------
+        TYPE
+            The number of failed transactions count.
+
+        """
+        return super().handle_request(self.NETWORK_FAILED_TOKENS_TRANSFERRED_COUNT, query_params)
