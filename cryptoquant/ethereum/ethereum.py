d3abdca7e43bf7c3fa35c6f200b115a227774033
Lautaro Parada <lautaro.parada.opazo@gmail.com>
2025-10-17 13:33:47 -0300

    failed trx count

--- cryptoquant/ethereum/ethereum.py
+++ cryptoquant/ethereum/ethereum.py
@@ -68,6 +68,7 @@ class Ethereum(RequestHandler):
         self.NETWORK_TOKENS_TRANSFERRED_BY_CONTRACT_CALLS_INTERNAL = "eth/network-data/tokens-transferred-by-contract-calls-internal"
         self.NETWORK_TOKENS_TRANSFERRED_BY_CONTRACT_CALLS = "eth/network-data/tokens-transferred-by-contract-calls"
         self.NETWORK_TOKENS_TRANSFERRED_ALL = "eth/network-data/tokens-transferred-all"
+        self.NETWORK_FAILED_TRANSACTIONS_COUNT = "eth/network-data/failed-transactions-count"      
         
         super().__init__(api_key)
         
@@ -2187,3 +2188,43 @@ class Ethereum(RequestHandler):
 
         """
         return super().handle_request(self.NETWORK_TOKENS_TRANSFERRED_ALL, query_params)
+    
+    def get_eth_ntx_failed_trx_count(self, **query_params):
+        """
+        This endpoint returns metrics related to the number of failed 
+        transactions. We provide failed_transactions_count_total metric, the
+        total number of failed transactions.
+
+        Parameters
+        ----------
+        **query_params : TYPE
+            window (str, optional): day, hour, 10minute, and block.
+            from_ (any, optional): This defines the starting time for which data
+                                will be gathered, formatted as YYYYMMDDTHHMMSS 
+                                (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                                If window=day is used, it can also be formatted 
+                                as YYYYMMDD (date). If window=block is used, you
+                                can also specify the exact block height (e.g. 510000). 
+                                If this field is not specified, response will 
+                                include data from the earliest time.
+           to_ (any, optinal): This defines the ending time for which data will
+                               be gathered, formatted as YYYYMMDDTHHMMSS 
+                               (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                               If window=day is used, it can also be formatted 
+                               as YYYYMMDD (date). If window=block is used, you
+                               can also specify the exact block height (e.g. 510000).
+                               If this field is not specified, response will 
+                               include data from the latest time
+           limit (int, optional): The maximum number of entries to return before
+                                  the latest data point (or before to if specified).
+                                  This field ranges from 1 to 100,000.
+           format (str, optional): A format type about return message type. 
+                                   Supported formats are json, csv.
+
+        Returns
+        -------
+        dict
+            The number of failed transactions count.
+
+        """
+        return super().handle_request(self.NETWORK_FAILED_TRANSACTIONS_COUNT, query_params)
