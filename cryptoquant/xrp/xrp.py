eb9bb3685a5d1b8db4a20762f02cc1854ba60480
Lautaro Parada <lautaro.parada.opazo@gmail.com>
2025-10-17 18:32:11 -0300

    entity reserve

--- cryptoquant/xrp/xrp.py
+++ cryptoquant/xrp/xrp.py
@@ -13,6 +13,9 @@ class XRP(RequestHandler):
         
         # Entity List
         self.ENTITY_LIST = "xrp/status/entity-list"
+        # Entity flows
+        self.ENTITY_RESERVE = "xrp/entity-flows/reserve"
+        
         
     # -----------------------------------
     # Entity list
@@ -36,3 +39,49 @@ class XRP(RequestHandler):
 
         """
         return super().handle_request(self.ENTITY_LIST, query_params)
+    
+    # -----------------------------------
+    # Entity flows
+    # -----------------------------------
+    
+    def get_xrp_entity_reserve(self, **query_params):
+        """
+        This namespace contains endpoints to retrieve metrics related to XRP 
+        Entity Flows. Currently, We only supports Exchanges for available 
+        entities. Supported Exchanges: Binance, Bitfinex, Bitget, Bithumb, 
+        Bitstamp, Bybit, Gate.io, HTX Global, Kucoin, OKX, Upbit
+
+        Parameters
+        ----------
+        **query_params : TYPE
+            exchange (str, required): An exchange supported by CryptoQuant.
+            window (str, optional): day, hour, and 10min.
+            from_ (any, optional): This defines the starting time for which data
+                                will be gathered, formatted as YYYYMMDDTHHMMSS 
+                                (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                                If window=day is used, it can also be formatted 
+                                as YYYYMMDD (date). If window=block is used, you
+                                can also specify the exact block height (e.g. 510000). 
+                                If this field is not specified, response will 
+                                include data from the earliest time.
+           to_ (any, optinal): This defines the ending time for which data will
+                               be gathered, formatted as YYYYMMDDTHHMMSS 
+                               (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                               If window=day is used, it can also be formatted 
+                               as YYYYMMDD (date). If window=block is used, you
+                               can also specify the exact block height (e.g. 510000).
+                               If this field is not specified, response will 
+                               include data from the latest time
+           limit (int, optional): The maximum number of entries to return before
+                                  the latest data point (or before to if specified).
+                                  This field ranges from 1 to 100,000.
+           format (str, optional): A format type about return message type. 
+                                   Supported formats are json, csv
+
+        Returns
+        -------
+        dict
+            The amount of XRP on a given entity on this window.
+
+        """
+        return super().handle_request(self.ENTITY_RESERVE, query_params)
