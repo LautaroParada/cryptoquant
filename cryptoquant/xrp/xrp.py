f514623dfcfc306a95bcd459705f0b93654f90a2
Lautaro Parada <lautaro.parada.opazo@gmail.com>
2025-10-17 21:10:35 -0300

    Update xrp.py

--- cryptoquant/xrp/xrp.py
+++ cryptoquant/xrp/xrp.py
@@ -44,6 +44,7 @@ class XRP(RequestHandler):
         self.NETWORK_FEES = "xrp/network-data/fees"
         self.NETWORK_TRANSACTIONS_COUNT = "xrp/network-data/transactions-count"
         self.NETWORK_TOKENS_TRANSFERRED = "xrp/network-data/tokens-transferred"
+        self.NETWORK_SUPPLY = "xrp/network-data/supply"
         
     # -----------------------------------
     # Entity list
@@ -1200,3 +1201,41 @@ class XRP(RequestHandler):
 
         """
         return super().handle_request(self.NETWORK_TOKENS_TRANSFERRED, query_params)
+    
+    def get_xrp_ntx_supply(self, **query_params):
+        """
+        This end point returns total supply of XRP.
+
+        Parameters
+        ----------
+        **query_params : TYPE
+            window (str, optional): day.
+            from_ (any, optional): This defines the starting time for which data
+                                will be gathered, formatted as YYYYMMDDTHHMMSS 
+                                (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                                If window=day is used, it can also be formatted 
+                                as YYYYMMDD (date). If window=block is used, you
+                                can also specify the exact block height (e.g. 510000). 
+                                If this field is not specified, response will 
+                                include data from the earliest time.
+           to_ (any, optinal): This defines the ending time for which data will
+                               be gathered, formatted as YYYYMMDDTHHMMSS 
+                               (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                               If window=day is used, it can also be formatted 
+                               as YYYYMMDD (date). If window=block is used, you
+                               can also specify the exact block height (e.g. 510000).
+                               If this field is not specified, response will 
+                               include data from the latest time
+           limit (int, optional): The maximum number of entries to return before
+                                  the latest data point (or before to if specified).
+                                  This field ranges from 1 to 100,000.
+           format (str, optional): A format type about return message type. 
+                                   Supported formats are json, csv.
+
+        Returns
+        -------
+        dict
+            Total amount of XRP available.
+
+        """
+        return super().handle_request(self.NETWORK_SUPPLY, query_params)
