4b1455d297742a1111cfe0fb090a68bf55a8dc5d
Lautaro Parada <lautaro.parada.opazo@gmail.com>
2025-10-20 10:14:28 -0300

    ohlcv

--- cryptoquant/stablecoins/stablecoins.py
+++ cryptoquant/stablecoins/stablecoins.py
@@ -22,6 +22,8 @@ class StableCoins(RequestHandler):
         self.EXCH_ADDRESSES_COUNT = "stablecoin/exchange-flows/addresses-count"
         # Flow Indicator
         self.FLOW_EXCHANGE_SUPPLY_RATIO = "stablecoin/flow-indicator/exchange-supply-ratio"
+        # Price OHLCV
+        self.MARKET_OHLCV = "stablecoin/market-data/price-ohlcv"
         
     # -----------------------------
     # Entity List
@@ -356,3 +358,56 @@ class StableCoins(RequestHandler):
 
         """
         return super().handle_request(self.FLOW_EXCHANGE_SUPPLY_RATIO, query_params)
+    
+    # -----------------------------
+    # Flow Indicator
+    # -----------------------------
+    
+    def get_stable_mkt_ohlcv(self, **query_params):
+        """
+        This endpoint returns metrics related to Stablecoin's Index Price.
+        Price OHLCV data consists of five metrics.  open, the opening price at 
+        the beginning of the window, close, USD closing price at the end of the 
+        window,  high, the highest USD price in a given window, low, the lowest 
+        USD price in a given window, and volume, the total token volume traded 
+        in a given window.
+        
+        At this endpoint, metrics are calculated by Minute, Hour and Day.
+        Stablecoin Index Price is calculated by taking VWAP(Volume Weighted 
+        Average Price) of Stablecoin price data aggregated from global
+        exchanges. 
+
+        Parameters
+        ----------
+        **query_params : TYPE
+            token (str, required): A stablecoin from the table that CQ support.
+            window (str, optional): day, hour, and min.
+            from_ (any, optional): This defines the starting time for which data
+                                will be gathered, formatted as YYYYMMDDTHHMMSS 
+                                (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                                If window=day is used, it can also be formatted 
+                                as YYYYMMDD (date). If window=block is used, you
+                                can also specify the exact block height (e.g. 510000). 
+                                If this field is not specified, response will 
+                                include data from the earliest time.
+           to_ (any, optinal): This defines the ending time for which data will
+                               be gathered, formatted as YYYYMMDDTHHMMSS 
+                               (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                               If window=day is used, it can also be formatted 
+                               as YYYYMMDD (date). If window=block is used, you
+                               can also specify the exact block height (e.g. 510000).
+                               If this field is not specified, response will 
+                               include data from the latest time
+           limit (int, optional): The maximum number of entries to return before
+                                  the latest data point (or before to if specified).
+                                  This field ranges from 1 to 100,000.
+           format (str, optional): A format type about return message type. 
+                                   Supported formats are json, csv
+
+        Returns
+        -------
+        dict
+            Price OHLCV data.
+
+        """
+        return super().handle_request(self.MARKET_OHLCV, query_params)
