9588226231e37411b078c1ce9166c322cf9cb52e
Lautaro Parada <lautaro.parada.opazo@gmail.com>
2025-10-20 10:08:40 -0300

    exchange supply ratio

--- cryptoquant/stablecoins/stablecoins.py
+++ cryptoquant/stablecoins/stablecoins.py
@@ -20,6 +20,8 @@ class StableCoins(RequestHandler):
         self.EXCH_OUTFLOW = "stablecoin/exchange-flows/outflow"
         self.EXCH_TRANSACTIONS_COUNT = "stablecoin/exchange-flows/transactions-count"
         self.EXCH_ADDRESSES_COUNT = "stablecoin/exchange-flows/addresses-count"
+        # Flow Indicator
+        self.FLOW_EXCHANGE_SUPPLY_RATIO = "stablecoin/flow-indicator/exchange-supply-ratio"
         
     # -----------------------------
     # Entity List
@@ -50,6 +52,10 @@ class StableCoins(RequestHandler):
 
         """
         return super().handle_request(self.ENTITY_LIST, query_params)
+    
+    # -----------------------------
+    # Exchange Flows
+    # -----------------------------
     
     def get_stable_exch_reserve(self, **query_params):
         """
@@ -303,3 +309,50 @@ class StableCoins(RequestHandler):
 
         """
         return super().handle_request(self.EXCH_ADDRESSES_COUNT, query_params)
+    
+    # -----------------------------
+    # Flow Indicator
+    # -----------------------------
+    
+    def get_stable_flow_exch_supply_ratio(self, **query_params):
+        """
+        Exchange Supply Ratio is calculated as exchange reserve divided by 
+        total supply. The metric measures how much tokens are reserved in the 
+        exchange relative to total supply of the token.
+
+        Parameters
+        ----------
+        **query_params : TYPE
+            token (str, required): A stablecoin from the table that CQ support.
+            exchange (str, required): An exchange supported by CryptoQuant.
+            window (str, optional): day, hour, and block.
+            from_ (any, optional): This defines the starting time for which data
+                                will be gathered, formatted as YYYYMMDDTHHMMSS 
+                                (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                                If window=day is used, it can also be formatted 
+                                as YYYYMMDD (date). If window=block is used, you
+                                can also specify the exact block height (e.g. 510000). 
+                                If this field is not specified, response will 
+                                include data from the earliest time.
+           to_ (any, optinal): This defines the ending time for which data will
+                               be gathered, formatted as YYYYMMDDTHHMMSS 
+                               (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                               If window=day is used, it can also be formatted 
+                               as YYYYMMDD (date). If window=block is used, you
+                               can also specify the exact block height (e.g. 510000).
+                               If this field is not specified, response will 
+                               include data from the latest time
+           limit (int, optional): The maximum number of entries to return before
+                                  the latest data point (or before to if specified).
+                                  This field ranges from 1 to 100,000.
+           format (str, optional): A format type about return message type. 
+                                   Supported formats are json, csv
+
+        Returns
+        -------
+        dict
+            Ratio of reserved token in the exchange relative to the total 
+            supply.
+
+        """
+        return super().handle_request(self.FLOW_EXCHANGE_SUPPLY_RATIO, query_params)
