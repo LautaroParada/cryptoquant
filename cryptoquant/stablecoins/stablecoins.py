66bb425b662a3f98dfaf6f6670e0ff3bbc2b7d6f
Lautaro Parada <lautaro.parada.opazo@gmail.com>
2025-10-20 09:36:05 -0300

    reserve

--- cryptoquant/stablecoins/stablecoins.py
+++ cryptoquant/stablecoins/stablecoins.py
@@ -13,6 +13,8 @@ class StableCoins(RequestHandler):
         
         # Entity List
         self.ENTITY_LIST = "stablecoin/status/entity-list"
+        # Exchange Flows
+        self.RESERVE = "stablecoin/exchange-flows/reserve"
         
     # -----------------------------
     # Entity List
@@ -43,3 +45,45 @@ class StableCoins(RequestHandler):
 
         """
         return super().handle_request(self.ENTITY_LIST, query_params)
+    
+    def get_stable_exch_reserve(self, **query_params):
+        """
+        This endpoint returns the full historical on-chain balance of 
+        Stablecoin exchanges.
+
+        Parameters
+        ----------
+        **query_params : TYPE
+            token (str, required): A stablecoin from the table that CQ support.
+            exchange (str, required): An exchange supported by CryptoQuant.
+            window (str, optional): day, hour, and block.
+            from_ (any, optional): This defines the starting time for which data
+                                will be gathered, formatted as YYYYMMDDTHHMMSS 
+                                (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                                If window=day is used, it can also be formatted 
+                                as YYYYMMDD (date). If window=block is used, you
+                                can also specify the exact block height (e.g. 510000). 
+                                If this field is not specified, response will 
+                                include data from the earliest time.
+           to_ (any, optinal): This defines the ending time for which data will
+                               be gathered, formatted as YYYYMMDDTHHMMSS 
+                               (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                               If window=day is used, it can also be formatted 
+                               as YYYYMMDD (date). If window=block is used, you
+                               can also specify the exact block height (e.g. 510000).
+                               If this field is not specified, response will 
+                               include data from the latest time
+           limit (int, optional): The maximum number of entries to return before
+                                  the latest data point (or before to if specified).
+                                  This field ranges from 1 to 100,000.
+           format (str, optional): A format type about return message type. 
+                                   Supported formats are json, csv
+
+
+        Returns
+        -------
+        dict
+            The amount of Stablecoin on a given exchange on this window.
+
+        """
+        return super().handle_request(self.RESERVE, query_params)
