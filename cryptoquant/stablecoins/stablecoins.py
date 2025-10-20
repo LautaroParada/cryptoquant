1ddea87a9e33cff706a19e9efecb46294693f10d
Lautaro Parada <lautaro.parada.opazo@gmail.com>
2025-10-20 10:02:25 -0300

    addresses count

--- cryptoquant/stablecoins/stablecoins.py
+++ cryptoquant/stablecoins/stablecoins.py
@@ -19,6 +19,7 @@ class StableCoins(RequestHandler):
         self.EXCH_INFLOW = "stablecoin/exchange-flows/inflow"
         self.EXCH_OUTFLOW = "stablecoin/exchange-flows/outflow"
         self.EXCH_TRANSACTIONS_COUNT = "stablecoin/exchange-flows/transactions-count"
+        self.EXCH_ADDRESSES_COUNT = "stablecoin/exchange-flows/addresses-count"
         
     # -----------------------------
     # Entity List
@@ -260,3 +261,45 @@ class StableCoins(RequestHandler):
 
         """
         return super().handle_request(self.EXCH_TRANSACTIONS_COUNT, query_params)
+    
+    def get_stable_exch_addrs_count(self, **query_params):
+        """
+        This endpoint returns the number of addresses involved in 
+        inflow/outflow transactions.
+
+        Parameters
+        ----------
+        **query_params : TYPE
+            token (str, required): A stablecoin from the table that CQ support.
+            exchange (str, required): An exchange supported by CryptoQuant.
+            window (str, optional): day, hour, and block.
+            from_ (any, optional): This defines the starting time for which data
+                                will be gathered, formatted as YYYYMMDDTHHMMSS 
+                                (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                                If window=day is used, it can also be formatted 
+                                as YYYYMMDD (date). If window=block is used, you
+                                can also specify the exact block height (e.g. 510000). 
+                                If this field is not specified, response will 
+                                include data from the earliest time.
+           to_ (any, optinal): This defines the ending time for which data will
+                               be gathered, formatted as YYYYMMDDTHHMMSS 
+                               (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                               If window=day is used, it can also be formatted 
+                               as YYYYMMDD (date). If window=block is used, you
+                               can also specify the exact block height (e.g. 510000).
+                               If this field is not specified, response will 
+                               include data from the latest time
+           limit (int, optional): The maximum number of entries to return before
+                                  the latest data point (or before to if specified).
+                                  This field ranges from 1 to 100,000.
+           format (str, optional): A format type about return message type. 
+                                   Supported formats are json, csv
+
+        Returns
+        -------
+        dict
+            The number of addresses evoking inflow/outflow transactions to
+            exchange wallets.
+
+        """
+        return super().handle_request(self.EXCH_ADDRESSES_COUNT, query_params)
