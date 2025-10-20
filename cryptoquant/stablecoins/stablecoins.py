196db0c3c6c3cbf28ef226eacdb031e568cf4bf8
Lautaro Parada <lautaro.parada.opazo@gmail.com>
2025-10-20 10:32:03 -0300

    events count

--- cryptoquant/stablecoins/stablecoins.py
+++ cryptoquant/stablecoins/stablecoins.py
@@ -27,6 +27,7 @@ class StableCoins(RequestHandler):
         self.MARKET_CAPITALIZATION = "stablecoin/market-data/capitalization"
         # Network Data
         self.NETWORK_SUPPLY = "stablecoin/network-data/supply"
+        self.NETWORK_EVENTS_COUNT = "stablecoin/network-data/events-count"
         
     # -----------------------------
     # Entity List
@@ -512,3 +513,47 @@ class StableCoins(RequestHandler):
 
         """
         return super().handle_request(self.NETWORK_SUPPLY, query_params)
+    
+    def get_stable_ntx_events_count(self, **query_params):
+        """
+        This endpoint returns metrics related to the number of events. CQ
+        provide several metrics. events_mint_count, events_issue_count, 
+        events_burn_count and events_redeem_count are metrics that represent 
+        the number of events related to each actions (mint, issue, burn, 
+        redeem). For further information about the actions, please refer to the
+        section Stablecoin Issuing Mechanism.
+
+        Parameters
+        ----------
+        **query_params : TYPE
+            token (str, required): A stablecoin from the table that CQ support.
+            window (str, optional): day and block.
+            from_ (any, optional): This defines the starting time for which data
+                                will be gathered, formatted as YYYYMMDDTHHMMSS 
+                                (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                                If window=day is used, it can also be formatted 
+                                as YYYYMMDD (date). If window=block is used, you
+                                can also specify the exact block height (e.g. 510000). 
+                                If this field is not specified, response will 
+                                include data from the earliest time.
+           to_ (any, optinal): This defines the ending time for which data will
+                               be gathered, formatted as YYYYMMDDTHHMMSS 
+                               (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                               If window=day is used, it can also be formatted 
+                               as YYYYMMDD (date). If window=block is used, you
+                               can also specify the exact block height (e.g. 510000).
+                               If this field is not specified, response will 
+                               include data from the latest time
+           limit (int, optional): The maximum number of entries to return before
+                                  the latest data point (or before to if specified).
+                                  This field ranges from 1 to 100,000.
+           format (str, optional): A format type about return message type. 
+                                   Supported formats are json, csv
+
+        Returns
+        -------
+        dict
+            Transfer, mint, issue, burn and redeem.
+
+        """
+        return super().handle_request(self.NETWORK_EVENTS_COUNT, query_params)
