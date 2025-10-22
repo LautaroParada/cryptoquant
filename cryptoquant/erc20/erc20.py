487184406e5bd4879fcc625453a62715eb421b5d
Lautaro Parada <lautaro.parada.opazo@gmail.com>
2025-10-22 14:29:45 -0300

    addresses count

--- cryptoquant/erc20/erc20.py
+++ cryptoquant/erc20/erc20.py
@@ -30,6 +30,7 @@ class Erc20(RequestHandler):
         self.NETWORK_TRANSACTIONS_COUNT = "erc20/network-data/transactions-count"
         self.NETWORK_TOKENS_TRANSFERRED_COUNT = "erc20/network-data/tokens-transferred-count"
         self.NETWORK_TOKENS_TRANSFERRED = "erc20/network-data/tokens-transferred"
+        self.NETWORK_ADDRESSES_COUNT = "erc20/network-data/addresses-count"
         
     # -------------------------------
     # Entity list
@@ -638,3 +639,48 @@ class Erc20(RequestHandler):
 
         """
         return super().handle_request(self.NETWORK_TOKENS_TRANSFERRED, query_params)
+    
+    def get_erc20_ntx_addrs_count(self, **query_params):
+        """
+        This endpoint returns metrics relating to the number of used to 
+        transfer the tokens. CQ provide several metrics, addresses_count_active, 
+        the total number of unique addresses that were active (either sender or
+        receiver) on the blockchain in a given window, addresses_count_sender, 
+        the number of addresses that were active as a sender, and 
+        addresses_count_receiver, the number of addresses that were active as a
+        receiver.
+
+        Parameters
+        ----------
+        **query_params : TYPE
+            token (str, required): A ERC20 token from the table that CQ support.
+            window (str, optional): day, hour, 10min, and block.
+            from_ (any, optional): This defines the starting time for which data
+                                will be gathered, formatted as YYYYMMDDTHHMMSS 
+                                (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                                If window=day is used, it can also be formatted 
+                                as YYYYMMDD (date). If window=block is used, you
+                                can also specify the exact block height (e.g. 510000). 
+                                If this field is not specified, response will 
+                                include data from the earliest time.
+           to_ (any, optinal): This defines the ending time for which data will
+                               be gathered, formatted as YYYYMMDDTHHMMSS 
+                               (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                               If window=day is used, it can also be formatted 
+                               as YYYYMMDD (date). If window=block is used, you
+                               can also specify the exact block height (e.g. 510000).
+                               If this field is not specified, response will 
+                               include data from the latest time
+           limit (int, optional): The maximum number of entries to return before
+                                  the latest data point (or before to if specified).
+                                  This field ranges from 1 to 100,000.
+           format (str, optional): A format type about return message type. 
+                                   Supported formats are json, csv
+
+        Returns
+        -------
+        dict
+            Statistics for addresses in the network of the token.
+
+        """
+        return super().handle_request(self.NETWORK_ADDRESSES_COUNT, query_params)
