efb8c4c6f56e28ce5bb767af099c23f2e0f3d921
Lautaro Parada <lautaro.parada.opazo@gmail.com>
2025-10-22 14:17:11 -0300

    trx count

--- cryptoquant/erc20/erc20.py
+++ cryptoquant/erc20/erc20.py
@@ -27,6 +27,7 @@ class Erc20(RequestHandler):
         # Network data
         self.NETWORK_SUPPLY = "erc20/network-data/supply"
         self.NETWORK_VELOCITY = "erc20/network-data/velocity"
+        self.NETWORK_TRANSACTIONS_COUNT = "erc20/network-data/transactions-count"
         
     # -------------------------------
     # Entity list
@@ -505,3 +506,45 @@ class Erc20(RequestHandler):
 
         """
         return super().handle_request(self.NETWORK_VELOCITY, query_params)
+    
+    def get_erc20_ntx_trx_count(self, **query_params):
+        """
+        This endpoint returns metrics related to the number of transactions. CQ
+        provide several metrics, transactions_count_total, the total number of 
+        transactions, transactions_count_mean, the mean number of transactions.
+        Note all events executed by the ERC20 contract are counted.
+
+        Parameters
+        ----------
+        **query_params : TYPE
+            token (str, required): A ERC20 token from the table that CQ support.
+            window (str, optional): day, hour, 10min, and block.
+            from_ (any, optional): This defines the starting time for which data
+                                will be gathered, formatted as YYYYMMDDTHHMMSS 
+                                (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                                If window=day is used, it can also be formatted 
+                                as YYYYMMDD (date). If window=block is used, you
+                                can also specify the exact block height (e.g. 510000). 
+                                If this field is not specified, response will 
+                                include data from the earliest time.
+           to_ (any, optinal): This defines the ending time for which data will
+                               be gathered, formatted as YYYYMMDDTHHMMSS 
+                               (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                               If window=day is used, it can also be formatted 
+                               as YYYYMMDD (date). If window=block is used, you
+                               can also specify the exact block height (e.g. 510000).
+                               If this field is not specified, response will 
+                               include data from the latest time
+           limit (int, optional): The maximum number of entries to return before
+                                  the latest data point (or before to if specified).
+                                  This field ranges from 1 to 100,000.
+           format (str, optional): A format type about return message type. 
+                                   Supported formats are json, csv
+
+        Returns
+        -------
+        dict
+            Statistics for transactions count.
+
+        """
+        return super().handle_request(self.NETWORK_TRANSACTIONS_COUNT, query_params)
