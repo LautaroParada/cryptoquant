0c59cb4bfbcfa6103e1ad04f8971041e57ff71d6
Lautaro Parada <lautaro.parada.opazo@gmail.com>
2025-10-22 13:31:12 -0300

    outflow

--- cryptoquant/erc20/erc20.py
+++ cryptoquant/erc20/erc20.py
@@ -17,6 +17,7 @@ class Erc20(RequestHandler):
         self.EXCHANGE_FLOWS_RESERVE = "erc20/exchange-flows/reserve"
         self.EXCHANGE_FLOWS_NETFLOW = "erc20/exchange-flows/netflow"
         self.EXCHANGE_FLOWS_INFLOW = "erc20/exchange-flows/inflow"
+        self.EXCHANGE_FLOWS_OUTFLOW = "erc20/exchange-flows/outflow"
         
     # -------------------------------
     # Entity list
@@ -180,3 +181,46 @@ class Erc20(RequestHandler):
 
         """
         return super().handle_request(self.EXCHANGE_FLOWS_INFLOW, query_params)
+    
+    def get_erc20_exch_outflow(self, **query_params):
+        """
+        This endpoint returns the outflow of ERC20 token into exchange wallets
+        for as far back as we track. The average outflow is the average 
+        transaction value for transactions flowing into exchange wallets on a 
+        given day.
+
+        Parameters
+        ----------
+        **query_params : TYPE
+            token (str, required): A ERC20 token from the table that CQ support.
+            exchange (str, required): An exchange supported by CryptoQuant.
+            window (str, optional): day, hour, and block.
+            from_ (any, optional): This defines the starting time for which data
+                                will be gathered, formatted as YYYYMMDDTHHMMSS 
+                                (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                                If window=day is used, it can also be formatted 
+                                as YYYYMMDD (date). If window=block is used, you
+                                can also specify the exact block height (e.g. 510000). 
+                                If this field is not specified, response will 
+                                include data from the earliest time.
+           to_ (any, optinal): This defines the ending time for which data will
+                               be gathered, formatted as YYYYMMDDTHHMMSS 
+                               (indicating YYYY-MM-DDTHH:MM:SS, UTC time). 
+                               If window=day is used, it can also be formatted 
+                               as YYYYMMDD (date). If window=block is used, you
+                               can also specify the exact block height (e.g. 510000).
+                               If this field is not specified, response will 
+                               include data from the latest time
+           limit (int, optional): The maximum number of entries to return before
+                                  the latest data point (or before to if specified).
+                                  This field ranges from 1 to 100,000.
+           format (str, optional): A format type about return message type. 
+                                   Supported formats are json, csv
+
+        Returns
+        -------
+        dict
+            outflow statistics.
+
+        """
+        return super().handle_request(self.EXCHANGE_FLOWS_OUTFLOW, query_params)
