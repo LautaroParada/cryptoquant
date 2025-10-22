95ba143d1055798139e2f43d04fda12a43942965
Lautaro Parada <lautaro.parada.opazo@gmail.com>
2025-10-22 14:39:34 -0300

    Clase altcoins
    Base e implementación del unico endpoint par esta clase en la API de CQ. De momento solo tiene datos de mercado. Eventualemente agregaran más datos interesantes para evaluar.

--- cryptoquant/cryptoquant.py
+++ cryptoquant/cryptoquant.py
@@ -12,8 +12,10 @@ from cryptoquant.xrp import XRP
 from cryptoquant.stablecoins import StableCoins
 from cryptoquant.trx import TRX
 from cryptoquant.erc20 import Erc20
+from cryptoquant.altcoins import AltCoins
 
-class CryptoQuant(Discovery, Bitcoin, Ethereum, XRP, StableCoins, TRX, Erc20):
+class CryptoQuant(Discovery, Bitcoin, Ethereum, XRP, StableCoins, TRX, Erc20,
+                  AltCoins):
     def __init__(self, api_key):
         # Sub estructuras de la API
         Discovery.__init__(self, api_key)
@@ -23,3 +25,4 @@ class CryptoQuant(Discovery, Bitcoin, Ethereum, XRP, StableCoins, TRX, Erc20):
         StableCoins.__init__(self, api_key)
         TRX.__init__(self, api_key)
         Erc20.__init__(self, api_key)
+        AltCoins.__init__(self, api_key)
